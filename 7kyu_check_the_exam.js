bb68f6ae1b102644579cfce2519f3316a3433aeb
Steven Moyes <102743233+StevenMoyes@users.noreply.github.com>
2022-06-10 16:03:07 +0200

    codewars solutions for 10 june 2022

---
+++ 7kyu_check_the_exam.js
@@ -0,0 +1,17 @@
+function checkExam(array1, array2) {
+    let count = 0
+    
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] == array2[i]) {
+        count += 4
+      } else if (array2[i] == '') {
+        count += 0
+      } else {
+        count -= 1
+      }
+    } if (count < 0) {
+      return 0
+    } else {
+      return count
+    }
+  }
---
+++ 8kyu_terminal_game_combat_function.js
@@ -0,0 +1,9 @@
+function combat(health, damage) {
+    let result = health - damage
+    
+    if (result < 0) {
+      return 0
+    } else {
+      return result
+    }
+  }
